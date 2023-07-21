e49cd0e32c5dc0865f42c95b863b30c01497a75a
Elyas <Elyas.shamal2@gmail.com>
2023-07-21 14:20:24 -0400

    completed

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self,brand,size):
+        self.brand = brand
+        self.size = size
+
+    @property
+    def size(slef):
+        return slef.__size   
+    
+    @size.setter
+    def size(self, size):
+        if type(size) != int:
+            print('size must be an integer')
+        else:
+            self.__size = size   
+
+    def cobble(slef):
+        print('Your shoe is as good as new!')
+        slef.condition = 'New'        
